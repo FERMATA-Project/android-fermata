6ab7bd869c49be7eaed23ae63e128c09db6b6ec6
이다영 <dayoung@idayeong-ui-MacBookPro.local>
2021-08-08 21:11:46 +0900

    playlist now

---
+++ app/src/main/java/com/example/fermata/RetrofitAPI.java
@@ -0,0 +1,17 @@
+package com.example.fermata;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+import com.example.fermata.resData.MusicList;
+
+public interface RetrofitAPI {
+    // 현재 playList
+    //@POST("/...")
+    //Call<MusicList> ...
+}
+
+
+
---
+++ app/src/main/java/com/example/fermata/resData/MusicList.java
@@ -0,0 +1,8 @@
+package com.example.fermata.resData;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MusicList {
+ //...
+
+}
