455eecf86afe0fc4eacb6bccf7901cfa2e9929a2
name명 <dnri33@naver.com>
2021-08-10 04:46:45 +0900

    210810

--- app/src/main/java/com/example/fermata/RetrofitAPI.java
+++ app/src/main/java/com/example/fermata/RetrofitAPI.java
@@ -1,7 +1,7 @@
 package com.example.fermata;
 
-import com.example.fermata.domain.Music;
 import com.example.fermata.response.musicResponse;
+import com.example.fermata.response.playlistResponse;
 
 import retrofit2.Call;
 import retrofit2.http.POST;
@@ -22,4 +22,12 @@ public interface RetrofitAPI {
     // 현재 playList
     //@POST("/...")
     //Call<MusicList> ...
+
+    // 최근 재생목록 5개 API
+    @POST("/music/playlist_lately")
+    Call<musicResponse> requestPlaylistLately();
+
+    // 재생목록 리스트 API
+    @POST("/playlist/playlist_list")
+    Call<playlistResponse> requestPlaylistList();
 }
